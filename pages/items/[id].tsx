530bdbaec9cddf81d153120a20e06407393dd7bb
unsu0707 <unsu0707@gmail.com>
2022-01-20 14:57:30 +0900

    Make Item Detail Page with tailwind css

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -2,28 +2,32 @@ import { NextPage } from "next";
 
 const ItemDetail: NextPage = () => {
   return (
-    <div>
-      <div>
-        <div />
-        <div>
-          <div />
+    <div className="px-4 py-10">
+      <div className="mb-8">
+        <div className="h-96 bg-slate-300" />
+        <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
+          <div className="w-12 h-12 rounded-full bg-slate-300" />
           <div>
-            <p>Stephen Joves</p>
-            <p>View Profile &rarr;</p>
+            <p className="text-sm font-medium teext-gray-700">Stephen Joves</p>
+            <p className="text-xs font-medium text-gray-500">
+              View Profile &rarr;
+            </p>
           </div>
         </div>
-        <div>
-          <h1>Galaxy S100</h1>
-          <p>$150</p>
-          <p>
+        <div className="mt-5">
+          <h1 className="text-3xl font-bold text-gray-900">Galaxy S100</h1>
+          <span className="text-3xl block mt-3 text-gray-900">$150</span>
+          <p className="my-6 text-gray-700">
             Smooth like butter Like a criminal undercover Gon’ pop like trouble
             Breakin’ into your heart like that Cool shade stunner Yeah I owe it
             all to my mother. Hot like summer Yeah I’m makin’ you sweat like
             that Break it down
           </p>
-          <div>
-            <button>Talk to Seller</button>
-            <button>
+          <div className="flex items-center justify-between space-x-2">
+            <button className="flex-1 bg-orange-500 text-white py-3 rounded-md focus-outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 font-medium hover:bg-orange-600">
+              Talk to Seller
+            </button>
+            <button className="p-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500">
               <svg
                 className="h-6 w-6 "
                 xmlns="http://www.w3.org/2000/svg"
@@ -44,13 +48,13 @@ const ItemDetail: NextPage = () => {
         </div>
       </div>
       <div>
-        <h2>Similar Items</h2>
-        <div>
+        <h2 className="text-2xl font-bold text-gray-900">Similar Items</h2>
+        <div className="mt-6 grid grid-cols-2 gap-4">
           {[1, 2, 3, 4, 5, 6].map((_, i) => (
             <div key={i}>
-              <div />
-              <h3>Galaxy S75</h3>
-              <p>$75</p>
+              <div className="h-56 w-full mb-4 bg-slate-300" />
+              <h3 className="text-gray-700 -mb-1">Galaxy S75</h3>
+              <span className="text-sm font-medium text-gray-900">$75</span>
             </div>
           ))}
         </div>
